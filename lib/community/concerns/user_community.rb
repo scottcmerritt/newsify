79effb5f08e25a3d242a68b060372cd62a72c781
Scott Merritt <scott.merritt@gmail.com>
2021-10-22 13:00:22 -0700

    updated bug

--- lib/community/concerns/user_community.rb
+++ lib/community/concerns/user_community.rb
@@ -2,6 +2,14 @@ module Community
   module UserCommunity
     extend ActiveSupport::Concern
 
+=begin
+  # currently in Feedbacker::UserInfo
+    scope :spam, -> { where("(removed = ? AND is_spam = ?)",false,true) }
+    scope :not_spam, -> { where("(removed = ? AND (is_spam is null OR is_spam = ?) )",false,false) }
+    scope :confirmed, -> { where("(NOT confirmed_at is null)")}
+    scope :not_confirmed, -> { where("(confirmed_at is null)")}
+=end    
+
     def interests_total
       Newsify::ItemInterest.where(user_id: self.id).count
     end
@@ -49,6 +57,49 @@ module Community
       #Cache.exists? online_key
       Newsify::Cache.time_to_live(online_key) > 0 
     end
+
+
+
+
+    #def online?
+    #  Impression.where("user_id = ? AND created_at > ?",self.id,10.minutes.ago).exists?
+    #end
+
+=begin  
+    # currently in Feedbacker::UserInfo
+
+    def total_views
+      Impression.where("user_id = ?",self.id).count
+    end
+    def last_view
+      Impression.where("user_id = ?",self.id).order("created_at DESC").first.created_at unless Impression.where("user_id = ?",self.id).order("created_at DESC").first.nil?
+    end
+   
+
+    def last_activity
+      [self.last_view,self.current_sign_in_at,self.last_sign_in_at,self.created_at,self.updated_at].compact.max
+  #    (!user.current_sign_in_at.nil? && (user.current_sign_in_at > minutes_ago.minutes.ago)) || (!user.last_sign_in_at.nil? && (user.last_sign_in_at > minutes_ago.minutes.ago) )
+
+    end
+    
+   
+    def self.order_by_ids(ids)
+      t = User.arel_table
+      condition = Arel::Nodes::Case.new(t[:id])
+      ids.each_with_index do |id, index|
+        condition.when(id).then(index)
+      end
+      order(condition)
+    end
+
+    def self.active_users minutes_ago: 60
+      user_list = User.not_spam.each.collect{|user| user if user.online? || (user.last_activity > minutes_ago.minutes.ago) }.compact.sort_by{|user| user.last_activity}.reverse
+      ids = user_list.pluck(:id)
+      User.where(id: ids).order_by_ids(ids)
+    end
+=end    
+
+
 
     
     def online! room_id: nil
